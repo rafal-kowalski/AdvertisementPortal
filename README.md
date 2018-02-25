508e0c824d5c3649344d56814f7d7b0b891f183a
Rafał <>
2018-02-25 19:24:11 +0100

    Initial project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+#Advertisement portal
+Side project to fiddle with Spring, Kotlin and Angular
---
+++ src/main/java/com/example/rk/AdvertisementPortalApp.java
@@ -0,0 +1,11 @@
+package com.example.rk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisementPortalApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AdvertisementPortalApp.class);
+    }
+}
